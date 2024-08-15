cb1b80326c118ec601dd0c1f71d396843cec7112
Yuvraj Singh <139317183+YuvrajGill01@users.noreply.github.com>
2024-08-15 16:05:59 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "805e57d86d3f343cb630bc918355005a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
